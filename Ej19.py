bdf53a28c07a7e685f16c9433c94095bb22ef302
Leiner2024 <136385391+Leiner2024@users.noreply.github.com>
2023-12-13 09:14:54 +0100

    Update Ej19.py

--- Ej19.py
+++ Ej19.py
@@ -1,13 +1,14 @@
+# Definimos una función llamada 'es_palindrom' para verificar si una palabra es un palíndromo.
 def es_palindrom(palabra):
-    #Convertimos todas los carácteres minúsculas para considerar la mayúscula y minúsculas equivalentes.
+    # Convertimos todos los caracteres a minúsculas para considerar mayúsculas y minúsculas como equivalentes.
     palabra = palabra.lower()
     return palabra == palabra[::-1]
 
-#Ejmplos de uso.
+# Ejemplos de uso.
 print(es_palindrom("radar"))   
 print(es_palindrom("air"))     
 print(es_palindrom("civic"))  
 print(es_palindrom("moix"))  
 print(es_palindrom("tapat"))   
 print(es_palindrom("casa"))   
-print(es_palindrom("refer")) 
+print(es_palindrom("refer"))
