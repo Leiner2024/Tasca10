92499592697642a5e8dc2bb8931155ef332827cf
Leiner2024 <136385391+Leiner2024@users.noreply.github.com>
2023-12-13 09:17:50 +0100

    Update Ej21.py

--- Ej21.py
+++ Ej21.py
@@ -1,6 +1,10 @@
+# Definimos una función llamada 'crear_repetits' para crear una cadena repetida 'n' veces
 def crear_repetits(n, c):
     return c * n
 
-# Programa Principal
+# En el programa principal:
+# Llamamos a la función 'crear_repetits' para crear una cadena repetida 6 veces con el carácter "a"
 resultado = crear_repetits(6, "a")
+
+# Mostramos el resultado
 print(resultado)
