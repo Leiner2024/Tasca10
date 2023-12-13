ff336483dde804a35e14a74c8c8e1c64f98e36be
Leiner2024 <136385391+Leiner2024@users.noreply.github.com>
2023-12-13 09:16:42 +0100

    Update Ej20.py

--- Ej20.py
+++ Ej20.py
@@ -1,9 +1,15 @@
-def superposición(lista1, lista2):
+# Definimos una función llamada 'superposicion' para verificar si hay superposición entre dos listas.
+def superposicion(lista1, lista2):
+    # Convertimos ambas listas a conjuntos y verificamos si tienen elementos en común
     return bool(set(lista1) & set(lista2))
-#Pp
+
+# En el programa principal:
+# Creamos dos listas de ejemplo
 lista_x = [6, 7, 8, 9]
 lista_z = [777, 69, 6, 50]
 
-respuesta = superposición(lista_x, lista_z)
+# Llamamos a la función 'superposicion' para verificar si hay superposición entre las listas
+respuesta = superposicion(lista_x, lista_z)
 
+# Mostramos el resultado
 print(respuesta)
