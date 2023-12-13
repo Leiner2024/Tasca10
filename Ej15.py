5733e9fe5fe28a22f32e65024cd6e42722fb03b5
Leiner2024 <136385391+Leiner2024@users.noreply.github.com>
2023-12-13 09:11:37 +0100

    Update Ej15.py

--- Ej15.py
+++ Ej15.py
@@ -1,6 +1,12 @@
+# Definimos una función llamada 'calcular_longitud' para casualmente calcular la longitud de una lista
 def calcular_longitud(y):
     return len(y)
-lista = [1, 99,44,55,77]
 
+# Creamos una lista.
+lista = [1, 99, 44, 55, 77]
+
+# Llamamos a la función 'calcular_longitud' para obtener la longitud de la lista
 longitud_de_lista = calcular_longitud(lista)
-print(f"La longitud de la lista es: {longitud_de_lista} ")
+
+# Mostramos el resultado
+print(f"La longitud de la lista es: {longitud_de_lista}")
