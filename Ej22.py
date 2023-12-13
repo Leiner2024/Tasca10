3b1b9d142eb16f19efbf23e2cb611c74a145e45a
Leiner2024 <136385391+Leiner2024@users.noreply.github.com>
2023-12-13 09:18:44 +0100

    Update Ej22.py

--- Ej22.py
+++ Ej22.py
@@ -1,6 +1,8 @@
+# Definimos una función llamada 'crear_puntos' para imprimir puntos en líneas según la lista dada.
 def crear_puntos(l):
     for n in l:
         print("." * n)
 
-#Program principal
+# En el programa principal:
+# Llamamos a la función 'crear_puntos' para imprimir puntos en líneas según la lista [1, 2, 3].
 crear_puntos([1, 2, 3])
