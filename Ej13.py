927fdb6e2d28d22ed87dea49d5ecd0283c1bbe05
Leiner2024 <136385391+Leiner2024@users.noreply.github.com>
2023-12-13 09:03:41 +0100

    Update Ej13.py

--- Ej13.py
+++ Ej13.py
@@ -1,9 +1,14 @@
+# Definimos una función llamada 'Messi' para imprimir el número mayor entre dos valores
 def Messi(a, b):
-    if a>b:
+    if a > b:
         print(a)
     else:
         print(b)
-#Programari principal
-a= int(input("Introdueix el primer nombre: "))
-b= int(input("Introdueix el segon nombre: "))
+
+# En el programa principal:
+# Le pedimos al usuario que introduzca dos números
+a = int(input("Introduce el primer número: "))
+b = int(input("Introduce el segundo número: "))
+
+# Llamamos a la función 'Messi' para imprimir el número mayor
 Messi(a, b)
